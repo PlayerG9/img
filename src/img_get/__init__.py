e649314d4aa51770b7d90b4109ceb99348f5a42b
PlayerG9 <g9yout@gmail.com>
2023-01-10 00:20:45 +0100

    added first version if the img-get command

--- src/img_get/__init__.py
+++ src/img_get/__init__.py
@@ -26,3 +26,88 @@ SOFTWARE.
 """
 __version_info__ = (0, 1, 0)
 __version__ = ".".join(str(_) for _ in __version_info__)
+
+import os
+import re
+import urllib.parse
+import requests
+from .util import printProgressBar
+
+
+class HTTPError404(Exception):
+    pass
+
+
+def img_get(url: str):
+    for iurl in url_iter(url):
+        try:
+            download(iurl)
+        except HTTPError404:
+            if url == iurl:
+                raise
+            return
+
+
+def url_iter(url: str):
+    yield url
+
+    match = list(re.finditer(r"\d+", url))[-1]
+    start, stop = match.start(), match.end()
+    number_string = match.group()
+    str_length = len(number_string)
+    number = int(number_string)
+
+    before, after = url[:start], url[stop:]
+
+    while True:
+        number += 1
+        yield f"{before}{str(number).rjust(str_length, '0')}{after}"
+
+
+def download(url: str):
+    print("Download", url)
+    response = requests.get(url=url, timeout=(20, None), stream=True)
+    if response.status_code == 404:
+        raise HTTPError404()
+    response.raise_for_status()
+
+    try:
+        content_size = response.headers['Content-length']
+    except KeyError:
+        content_size = None
+    else:
+        content_size = int(content_size)
+
+    filename = get_filename(url=url, response=response)
+    print(f"Writing to {filename}...")
+
+    try:
+        with open(filename, 'wb') as file:
+            progress = 0
+            for chunk in response.iter_content():
+                file.write(chunk)
+                progress += len(chunk)
+                printProgressBar(progress, content_size or 1, length=70)  # terminal size is 80 so reduced to 70
+    except Exception:
+        if os.path.isfile(filename):
+            os.remove(filename)
+        raise
+
+
+def get_filename(url: str, response: requests.Response):
+    fn = extract_filename(url=url, response=response)
+    i = 0
+    name, ext = os.path.splitext(fn)
+    while os.path.isfile(fn):
+        i += 1
+        fn = f"{name} ({i}){ext}"
+    return fn
+
+
+def extract_filename(url: str, response: requests.Response):
+    try:
+        content_disposition = response.headers['Content-Disposition']
+        return re.findall("filename=(.+)", content_disposition)[0]
+    except KeyError:
+        parsed = urllib.parse.urlparse(url=url)
+        return os.path.split(parsed.path)[1]
--- src/img_get/__main__.py
+++ src/img_get/__main__.py
@@ -5,10 +5,12 @@ r"""
 """
 import sys
 import argparse
-from . import __version__
+from . import __version__, img_get
 
 
 class NameSpace:
+    url: str
+
     def __repr__(self):
         return f"<args {self.__dict__}>"
 
@@ -16,10 +18,13 @@ class NameSpace:
 parser = argparse.ArgumentParser(prog="img-get", description=__doc__, add_help=True,
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-v', '--version', action="version", version=__version__)
+parser.add_argument('url', help="the url to start from")
 
 
 def main():
-    args = parser.parse_args(namespace=NameSpace)
+    args = parser.parse_args(namespace=NameSpace())
+    img_get(args.url)
+    return 0
 
 
 if __name__ == '__main__':
