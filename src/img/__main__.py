6e8556293cf916fdcbaabf2ccf0ffe8236c95710
PlayerG9 <g9yout@gmail.com>
2023-05-12 09:44:04 +0200

    updated main

--- src/img/__main__.py
+++ src/img/__main__.py
@@ -3,66 +3,86 @@
 r"""
 
 """
-import sys
-import typing as t
 import argparse
 from . import __version__
-
-
-class NameSpace:
-    url: str
-
-    # grabber
-    overwrite: bool
-    skips: int
-    formats: t.List[str]
-
-    # scraper
-    export: bool
-    max_depth: int
-    min_width: int
-    min_height: int
-
-    def __repr__(self):
-        return f"<args {self.__dict__}>"
+from . import grabber as lib_grabber
+from . import scraper as lib_scraper
+from . import updater as lib_updater
 
 
 parser = argparse.ArgumentParser(prog="img", description=__doc__, add_help=True,
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-v', '--version', action="version", version=__version__)
 
-grabber = parser.add_argument_group(
-    "grabber",
-    "Grab a collection of images. Used when passing an image-url"
-)
-grabber.add_argument('--overwrite', action=argparse.BooleanOptionalAction, default=False,
-                     help="whether or not to overwrite existing images")
-grabber.add_argument('--skips', type=int, default=0,
-                     help="sometimes images are missing. This is the amount of images are allowed to be skipped.")
-grabber.add_argument('-F', '--formats', type=lambda s: s.split(","), nargs='?', const="jpg,png", default=[],
-                     help=", seperated list of extensions to try if a [404] Not Found is returned")
+subparser = parser.add_subparsers(required=True)
 
-scraper = parser.add_argument_group(
-    "scraper",
-    "Scrape a website for images and get these images. Used when passing a website-url"
-)
-scraper.add_argument('--export', action="store_true", default=False,
-                     help="only print the found urls instead of downloading the images.")
-scraper.add_argument('-D', '--max-depth', type=int, default=1,
-                     help="how many linked pages to scrape")
-scraper.add_argument('-W', '--min-width', type=int, default=0,
-                     help="minimum width of images to download")
-scraper.add_argument('-H', '--min-height', type=int, default=0,
-                     help="minimum height of images to download")
+#
+# updater
+#
 
-parser.add_argument('url', help="the url to start from")
+update_parser = subparser.add_parser(
+    "update",
+    help="check for updates and install them",
+    description=lib_updater.__doc__,
+    # aliases=["upgrade"],
+)
+update_parser.set_defaults(cls=lib_updater.Updater)
+update_parser.add_argument("--check-only", action="store_true",
+                           help="only check for a new version but don't upgrade")
+
+
+#
+# grabber
+#
+
+grabber_parser = subparser.add_parser(
+    "grab",
+    help="Grab a collection of images",
+    description=lib_grabber.__doc__,
+    # aliases=["get"],
+)
+grabber_parser.set_defaults(cls=lib_grabber.ImageGrabber)
+grabber_parser.add_argument('--overwrite', action=argparse.BooleanOptionalAction, default=False,
+                            help="whether or not to overwrite existing images")
+grabber_parser.add_argument('--skips', type=int, default=0,
+                            help="sometimes images are missing."
+                                 "This is the amount of images are allowed to be skipped/missing.")
+grabber_parser.add_argument('-F', '--formats', type=lambda s: s.split(","), nargs='?', const="jpg,png", default=[],
+                            help=", seperated list of extensions to try if a [404] Not Found is returned")
+grabber_parser.add_argument('url', help="the url to start from")
+
+#
+# scraper
+#
+
+scraper_parser = subparser.add_parser(
+    "scrape",
+    help="Scrape a website for images and get these",
+    description=lib_scraper.__doc__,
+    # aliases=[],
+)
+scraper_parser.set_defaults(cls=lib_scraper.ImageScraper)
+scraper_parser.add_argument('--export', action="store_true", default=False,
+                            help="only print the found urls instead of downloading the images.")
+scraper_parser.add_argument('-D', '--max-depth', type=int, default=1,
+                            help="how many linked pages to scrape")
+scraper_parser.add_argument('-W', '--min-width', type=int, default=0,
+                            help="minimum width of images to download")
+scraper_parser.add_argument('-H', '--min-height', type=int, default=0,
+                            help="minimum height of images to download")
+scraper_parser.add_argument('url', help="the url to start from")
+
+#
+# main
+#
 
 
 def main():
-    args = parser.parse_args(namespace=NameSpace())
+    args = vars(parser.parse_args())
     print(args)
-    return 0
+    executor = args.pop("cls")(**args)
+    executor.run()
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    main()
