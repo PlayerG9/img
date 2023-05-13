2002f7714753ff27b6fa085ac7e5f01536161d3c
PlayerG9 <g9yout@gmail.com>
2023-05-13 18:16:59 +0200

    grabber-command setup

--- src/img/grabber/__init__.py
+++ src/img/grabber/__init__.py
@@ -3,7 +3,15 @@
 r"""
 
 """
+import typing as t
 
 
 class ImageGrabber:
-    pass
+    def __init__(self, url: str, overwrite: bool, skips: int, formats: t.List[str]):
+        self.url = url
+        self.overwrite = overwrite
+        self.skips = skips
+        self.formats = formats
+
+    def run(self):
+        raise NotImplementedError()
